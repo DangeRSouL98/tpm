5815cbaca9e3110e5a2173bc22624b8dc0753bac
Wellson <wellsonleewando0908@gmail.com>
2019-12-12 05:05:24 +0700

    commit pertama

---
+++ database/factories/FoodFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\food;
+use Faker\Generator as Faker;
+
+$factory->define(food::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
